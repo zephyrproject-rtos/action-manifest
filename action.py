ff75134fe74fb1f1c8a1da4e8261b21e006a94e2
Carles Cufi <carles.cufi@nordicsemi.no>
2024-12-08 20:27:35 +0100

    action: List both branches and tags a commit is pointed to
    Signed-off-by: Carles Cufi <carles.cufi@nordicsemi.no>

--- action.py
+++ action.py
@@ -182,11 +182,12 @@ def fmt_rev(repo, rev):
 
     try:
         if is_sha(rev):
-            branches = [f'`{b.name}`' for b in repo.get_branches() if rev ==
-                        b.commit.sha]
+            refs = [f'`{r.name}`' for r in (repo.get_branches() +
+                                            repo.get_tags()) if rev ==
+                                                                b.commit.sha]
             s = repo.get_commit(rev).html_url
             # commits get formatted nicely by GitHub itself
-            return s + f' ({",".join(branches)})' if len(branches) else s
+            return s + f' ({",".join(refs)})' if len(refs) else s
         elif rev in [t.name for t in repo.get_tags()]:
             # For some reason there's no way of getting the URL via API
             s = f'{repo.html_url}/releases/tag/{rev}'
