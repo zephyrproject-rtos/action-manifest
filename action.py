700ecf53942aba4433c070e5e58193f75c8eddde
Johan Hedberg <johan.hedberg@silabs.com>
2026-02-23 13:40:18 +0200

    action: Fix using deprecated GitHub auth API
    Fixes the following deprecation warning:
    
    action.py:457: DeprecationWarning: Argument login_or_token is deprecated,
    please use auth=github.Auth.Token(...) instead
    
    Signed-off-by: Johan Hedberg <johan.hedberg@silabs.com>

--- action.py
+++ action.py
@@ -18,7 +18,7 @@ from pathlib import Path
 # 3rd party imports go here
 import requests
 import yaml
-from github import Github, GithubException
+from github import Auth, Github, GithubException
 from github.File import File
 from github.PullRequest import PullRequest
 from github.Repository import Repository
@@ -458,7 +458,7 @@ def main():
         sys.exit('Github token not set in environment, please set the '
                  'GITHUB_TOKEN environment variable and retry.')
 
-    gh = Github(token)
+    gh = Github(auth=Auth.Token(token))
 
     org_str, repo_str, pr_str = gh_pr_split(args.pr)
     gh_repo = gh.get_repo(f'{org_str}/{repo_str}')
